4f72144a45bb84a965754de496eaf6875e613e4d
David Beauchamp <davidabeauchamp@gmail.com>
2022-12-02 22:54:28 -0800

    day2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,20 +1,3 @@
-const STRATEGY_GUIDE: [usize; 3] = [3, 6, 0];
-const SHAPE_POINTS:[usize; 3] = [1, 2, 3];
-
-fn to_index(c: &str) -> usize {
-    let index = match c {
-        "A" => 0,
-        "B" => 1,
-        "C" => 2,
-        "X" => 0,
-        "Y" => 1,
-        "Z" => 2,
-        _ => unreachable!("no index"),
-    };
-    return index;
-}
-
-
 struct Round<'a> {
     o: &'a str,
     p: &'a str,
@@ -22,16 +5,19 @@ struct Round<'a> {
 
 impl<'a> Round<'a> {
     fn score(&self) -> usize {
+        // X lose
+        // Y draw
+        // Z win
         return match (self.o, self.p) {
-            ("A", "X") => 1 + 3,
-            ("A", "Y") => 2 + 6,
-            ("A", "Z") => 3 + 0,
+            ("A", "X") => 3 + 0,
+            ("A", "Y") => 1 + 3,
+            ("A", "Z") => 2 + 6,
             ("B", "X") => 1 + 0,
             ("B", "Y") => 2 + 3,
             ("B", "Z") => 3 + 6,
-            ("C", "X") => 1 + 6,
-            ("C", "Y") => 2 + 0,
-            ("C", "Z") => 3 + 3,
+            ("C", "X") => 2 + 0,
+            ("C", "Y") => 3 + 3,
+            ("C", "Z") => 1 + 6,
             _ => unreachable!("impossible combination")
         }
     }
